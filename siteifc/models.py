60b0cb1cf8adcaeb276eda9729d9e19c884373ab
agathahelena <agatha.helena.so@gmail.com>
2025-09-09 11:38:38 -0300

    FEAT : Tema retirado

--- siteifc/models.py
+++ siteifc/models.py
@@ -5,7 +5,6 @@ from django.db import models
 class Eventos(models.Model):
 
     nome = models.CharField(max_length=80)
-    tema = models.CharField(max_length=80)
     descricao = models.CharField(max_length=5000)
     data = models.DateField
     hora = models.TimeField
